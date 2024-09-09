77f7ac1662e4fe9b08c03cc38b41205f26ee1551
santiago-caballero-jalafund <santicm95386b@gmail.com>
2024-09-08 22:25:34 -0400

    feat: last torunament view & firestore integration finished

---
+++ src/contexts/TournamentContext.tsx
@@ -0,0 +1,30 @@
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "../utils/firebase-config";
+import { Tournament, TournamentModality } from "../interfaces/interfaces";
+import imageTour from '../assets/bannerFsociety.jpg';
+
+export const fetchTournaments = async (): Promise<Tournament[]> => {
+  const tournamentsRef = collection(db, "tournaments");
+  const snapshot = await getDocs(tournamentsRef);
+  const tournaments = snapshot.docs.map((doc) => {
+    const data = doc.data();
+    
+    const tournament: Tournament = {
+      id: doc.id,
+      name: data.name || '',
+      imagePath: data.imagePath || imageTour,
+      inscriptionPrice: data.inscriptionPrice || 0,
+      participants: data.participants || 0,
+      teamLimit: data.teamLimit || 0,
+      modality: data.modality || TournamentModality.ELIMINATION,
+      startDate: data.startDate,
+      endDate: data.endDate,
+      teams: data.teams || [],
+      createdAt: data.createdAt,
+    };
+
+    return tournament;
+  });
+
+  return tournaments;
+};
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,19 +1,43 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import bannerApp from '../assets/backgroundSplash.jpg';
 import styles from "../assets/styles/home.module.css";
 import SliderHome from '../components/slider/SliderHome';
 import TournamentList from '../components/tournament/TournamentList';
-import {Tournaments} from '../utils/data';
 import Footer from '../components/Footer';
+import { fetchTournaments } from '../contexts/TournamentContext';
+import { Tournament } from '../interfaces/interfaces';
+import Splash from './Splash';
 
 const Home: React.FC = () => {
+  const [tournaments, setTournaments] = useState<Tournament[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    const loadTournaments = async () => {
+      try {
+        const data = await fetchTournaments();
+        setTournaments(data);
+      } catch (error) {
+        console.error("Error fetching tournaments:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    loadTournaments();
+  }, []);
+
+  if (loading) {
+    return <Splash/>;
+  }
+
   return (
     <main className={styles.homeContainer}>
       <img src={bannerApp} alt="banner app" className={styles.backgroundImage} />
       <div className={styles.homeContent}>
         <SliderHome />
         <h2 className={styles.subtitleHome}>TORNEOS ACTUALES</h2>
-        <TournamentList tournaments={Tournaments} />
+        <TournamentList tournaments={tournaments} />
       </div>
       <Footer/>
     </main>
