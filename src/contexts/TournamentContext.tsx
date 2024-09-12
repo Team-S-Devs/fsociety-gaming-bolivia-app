0f34300334be3ace2e6c51ac23e2ffddfae98191
santiago-caballero-jalafund <santicm95386b@gmail.com>
2024-09-12 00:31:13 -0400

    feat: modal finished

--- src/contexts/TournamentContext.tsx
+++ src/contexts/TournamentContext.tsx
@@ -2,31 +2,39 @@ import { collection, getDocs } from "firebase/firestore";
 import { db } from "../utils/firebase-config";
 import { Tournament, TournamentModality } from "../interfaces/interfaces";
 import imageTour from '../assets/bannerFsociety.jpg';
+import { Timestamp } from "firebase/firestore";
 
 export const fetchTournaments = async (): Promise<Tournament[]> => {
   const tournamentsRef = collection(db, "tournaments");
   const snapshot = await getDocs(tournamentsRef);
-  const tournaments = snapshot.docs.map((doc) => {
-    const data = doc.data();
-    
-    const tournament: Tournament = {
-      id: doc.id,
-      name: data.name || '',
-      description: data.description || '',
-      imagePath: data.imagePath || imageTour,
-      awards: data.awards || [],
-      inscriptionPrice: data.inscriptionPrice || 0,
-      participants: data.participants || 0,
-      teamLimit: data.teamLimit || 0,
-      modality: data.modality || TournamentModality.ELIMINATION,
-      startDate: data.startDate,
-      endDate: data.endDate,
-      teams: data.teams || [],
-      createdAt: data.createdAt,
-    };
+  
+  const now = Timestamp.now();
+  
+  const tournaments = snapshot.docs
+    .map((doc) => {
+      const data = doc.data();
+      
+      const tournament: Tournament = {
+        id: doc.id,
+        name: data.name || '',
+        description: data.description || '',
+        imagePath: data.imagePath || imageTour,
+        awards: data.awards || [],
+        inscriptionPrice: data.inscriptionPrice || 0,
+        participants: data.participants || 0,
+        teamLimit: data.teamLimit || 0,
+        modality: data.modality || TournamentModality.ELIMINATION,
+        startDate: data.startDate || now,
+        endDate: data.endDate || now,
+        teams: data.teams || [],
+        createdAt: data.createdAt || now,
+        deleted: data.deleted || false,
+      };
 
-    return tournament;
-  });
+      return tournament;
+    })
+    .filter(tournament => tournament.endDate.seconds >= now.seconds && !tournament.deleted);
+
 
   return tournaments;
 };
