13ad209a6eae509e4c129de32e4fe60ad1374190
just-fabian <fabian130404@gmail.com>
2024-09-15 13:35:54 -0400

    feat: add status active to tournament

--- src/components/BlurBoxContainer.tsx
+++ src/components/BlurBoxContainer.tsx
@@ -3,20 +3,24 @@ import { ReactNode } from "react";
 
 interface BlurBoxContainerProps {
   children: ReactNode;
+  padding?: { xs: string; sm: string; md: string };
 }
 
-const BlurBoxContainer: React.FC<BlurBoxContainerProps> = ({ children }) => {
+const BlurBoxContainer: React.FC<BlurBoxContainerProps> = ({
+  children,
+  padding = {
+    xs: "32px 20px",
+    sm: "48px 24px",
+    md: "56px",
+  },
+}) => {
   return (
     <Box
       sx={{
         background:
           "linear-gradient(rgba(94, 131, 249, 0.2), rgba(94, 131, 249, 0.2))",
         borderRadius: 10,
-        padding: {
-          xs: "32px 20px", 
-          sm: "48px 24px", 
-          md: "56px",      
-        },
+        padding,
         paddingBottom: "64px",
         paddingTop: "64px",
         backdropFilter: "blur(10px)",
--- src/components/tournament/admin/ViewTournaments.tsx
+++ src/components/tournament/admin/ViewTournaments.tsx
@@ -7,6 +7,12 @@ import {
   getPaginatedTournaments,
 } from "../../../utils/firebaseMethods";
 import Loader from "../../Loader";
+import { Button, Typography, useMediaQuery, useTheme } from "@mui/material";
+import BlurBoxContainer from "../../BlurBoxContainer";
+import { BiPlus } from "react-icons/bi";
+import { useNavigate } from "react-router-dom";
+import { PagesNames } from "../../../utils/constants";
+import styles from "../../../assets/styles/buttons.module.css";
 
 const ViewTournaments: React.FC = () => {
   const numPerPage = 10;
@@ -24,6 +30,11 @@ const ViewTournaments: React.FC = () => {
     undefined
   );
   const [loading, setLoading] = useState<boolean>(false);
+  const [error, setError] = useState<boolean>(false);
+
+  const navigate = useNavigate();
+  const theme = useTheme();
+  const isSmallScreen = useMediaQuery(theme.breakpoints.down("sm"));
 
   useEffect(() => {
     getNumPages("tournaments", numPerPage).then((result) => {
@@ -37,35 +48,71 @@ const ViewTournaments: React.FC = () => {
     const endBeforeDoc = direction === "prev" ? firstDoc : undefined;
 
     setLoading(true);
-    getPaginatedTournaments(
-      direction,
-      startAfterDoc,
-      endBeforeDoc,
-      numPerPage
-    ).then((data) => {
-      setTournaments(data.result);
-      setFirstDoc(data.firstDoc);
-      setLastDoc(data.lastDoc);
-    });
-    setLoading(false);
+    getPaginatedTournaments(direction, startAfterDoc, endBeforeDoc, numPerPage)
+      .then((data) => {
+        setTournaments(data.result);
+        setFirstDoc(data.firstDoc);
+        setLastDoc(data.lastDoc);
+        setLoading(false);
+      })
+      .catch(() => {
+        setError(true);
+        setLoading(false);
+      });
   }, [page, direction]);
+
+  const handleAdd = () => {
+    navigate(PagesNames.AdminAddTournament);
+  };
 
   return (
     <div>
-      {loading ? (
-        <Loader />
-      ) : (
-        <TournamentsTable
-          tournaments={tournaments}
-          page={page}
-          totalDocs={totalDocs}
-          setTournaments={setTournaments}
-          rowsPerPage={numPerPage}
-          setPage={setPage}
-          setDirection={setDirection}
-          pages={pages ?? 0}
-        />
-      )}
+      <BlurBoxContainer
+
+      >
+        <Typography variant="h5" mt={4}>
+          Torneos creados
+        </Typography>
+        <Button
+          variant="contained"
+          color="primary"
+          startIcon={<BiPlus />}
+          onClick={handleAdd}
+          fullWidth={isSmallScreen}
+          style={{ marginBottom: "16px" }}
+          className={styles.continueButton}
+        >
+          Añadir Torneo
+        </Button>
+        {loading ? (
+          <Loader />
+        ) : error ? (
+          <Typography
+            color="error"
+            variant="h6"
+            textAlign={"center"}
+            mt={4}
+            mb={3}
+          >
+            Error obteniendo los torneos. Por favor, recarga la página.
+          </Typography>
+        ) : tournaments.length > 0 ? (
+          <TournamentsTable
+            tournaments={tournaments}
+            page={page}
+            totalDocs={totalDocs}
+            setTournaments={setTournaments}
+            rowsPerPage={numPerPage}
+            setPage={setPage}
+            setDirection={setDirection}
+            pages={pages ?? 0}
+          />
+        ) : (
+          <Typography variant="h6" textAlign={"center"} mt={4} mb={3}>
+            No hay torneos creados.
+          </Typography>
+        )}
+      </BlurBoxContainer>
     </div>
   );
 };
