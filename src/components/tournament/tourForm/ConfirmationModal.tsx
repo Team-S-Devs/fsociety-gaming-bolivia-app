187b6282e56b228612244209c31aa01902d44d9a
santiago-caballero-jalafund <santicm95386@gmail.com>
2024-10-15 01:18:06 -0400

    feat: log out option from users with no team list

--- src/components/tournament/tourForm/ConfirmationModal.tsx
+++ src/components/tournament/tourForm/ConfirmationModal.tsx
@@ -6,32 +6,43 @@ interface ConfirmationModalProps {
   isOpen: boolean;
   onClose: () => void;
   onConfirm: () => void;
+  onExit: () => void;
   title: string;
   message: string;
   confirmText: string;
+  loading: boolean;
 }
 
 const ConfirmationModal: React.FC<ConfirmationModalProps> = ({
   isOpen,
   onClose,
   onConfirm,
+  onExit,
   title,
   message,
   confirmText,
+  loading,
 }) => {
   return (
     <JoinModal show={isOpen} onClose={onClose}>
       <div className={styles.modalForm}>
         <h3 className={styles.modalTitle}>{title}</h3>
         <p>{message}</p>
-        <div className={styles.modalButtons}>
-          <button onClick={onConfirm} className={styles.modalButton}>
-            {confirmText}
-          </button>
-          <button onClick={onClose} className={styles.modalButtonClose}>
-            Cancelar
-          </button>
-        </div>
+        {loading ? (
+          <div className={styles.loader}>Loading...</div>
+        ) : (
+          <div className={styles.modalButtons}>
+            <button onClick={onConfirm} className={styles.modalButton}>
+              {confirmText}
+            </button>
+            <button onClick={onExit} className={styles.modalButton}>
+              Salir
+            </button>
+            <button onClick={onClose} className={styles.modalButtonClose}>
+              Cancelar
+            </button>
+          </div>
+        )}
       </div>
     </JoinModal>
   );
